4dc239332d2ceb07f5c52a48ccbe71ee8b409cf2
Nonium <a.kasatkin@nonium.by>
2022-04-24 13:39:02 +0300

    develop

--- src/Models/Currency.php
+++ src/Models/Currency.php
@@ -2,58 +2,117 @@
 
 namespace Kaswell\NbrbBankApi\Models;
 
+use Carbon\Carbon;
 use Kaswell\NbnbApi\Abstracts\Model;
 
 /**
  * Class Currency
  * @package Kaswell\NbrbBankApi\Models
- *
- *
- * Cur_ID – внутренний код
- * Cur_ParentID – этот код используется для связи, при изменениях наименования, количества единиц к которому устанавливается курс белорусского рубля,
- *                буквенного, цифрового кодов и т.д. фактически одной и той же валюты*.
- * Cur_Code – цифровой код
- * Cur_Abbreviation – буквенный код
- * Cur_Name – наименование валюты на русском языке
- * Cur_Name_Bel – наименование на белорусском языке
- * Cur_Name_Eng – наименование на английском языке
- * Cur_QuotName – наименование валюты на русском языке, содержащее количество единиц
- * Cur_QuotName_Bel – наименование на белорусском языке, содержащее количество единиц
- * Cur_QuotName_Eng – наименование на английском языке, содержащее количество единиц
- * Cur_NameMulti – наименование валюты на русском языке во множественном числе
- * Cur_Name_BelMulti – наименование валюты на белорусском языке во множественном числе*
- * Cur_Name_EngMulti – наименование на английском языке во множественном числе*
- * Cur_Scale – количество единиц иностранной валюты
- * Cur_Periodicity – периодичность установления курса (0 – ежедневно, 1 – ежемесячно)
- * Cur_DateStart – дата включения валюты в перечень валют, к которым устанавливается официальный курс бел. рубля
- * Cur_DateEnd – дата исключения валюты из перечня валют, к которым устанавливается официальный курс бел. рубля
  */
 class Currency extends Model
 {
     /**
      * @var int
+     * внутренний код
      */
     protected int $id;
 
     /**
      * @var int
+     * этот код используется для связи, при изменениях наименования, количества
+     * единиц к которому устанавливается курс белорусского рубля,
+     * буквенного, цифрового кодов и т.д. фактически одной и той же валюты
      */
     protected int $parent_id;
 
+    /**
+     * @var int
+     * цифровой код
+     */
+    protected int $code;
 
-    protected $code;
+    /**
+     * @var string
+     * буквенный код
+     */
     protected string $abbreviation;
+
+    /**
+     * @var string
+     * наименование валюты на русском языке
+     */
     protected string $name;
+
+    /**
+     * @var string
+     * наименование на белорусском языке
+     */
     protected string $name_bel;
+
+    /**
+     * @var string
+     * наименование на английском языке
+     */
     protected string $name_eng;
+
+    /**
+     * @var string
+     * наименование валюты на русском языке, содержащее количество единиц
+     */
     protected string $quot_name;
+
+    /**
+     * @var string
+     * наименование на белорусском языке, содержащее количество единиц
+     */
     protected string $quot_name_bel;
+
+    /**
+     * @var string
+     * наименование на английском языке, содержащее количество единиц
+     *
+     */
     protected string $quot_name_eng;
+
+    /**
+     * @var string
+     * наименование валюты на русском языке во множественном числе
+     */
     protected string $name_multi;
+
+    /**
+     * @var string
+     * наименование валюты на белорусском языке во множественном числе
+     */
     protected string $name_multi_bel;
+
+    /**
+     * @var string
+     * наименование на английском языке во множественном числе
+     */
     protected string $name_multi_eng;
-    protected $scale;
-    protected $periodicity;
-    protected $date_start;
-    protected $date_end;
+
+    /**
+     * @var float
+     * количество единиц иностранной валюты
+     */
+    protected float $scale;
+
+    /**
+     * @var int
+     * периодичность установления курса (0 – ежедневно, 1 – ежемесячно)
+     */
+    protected int $periodicity;
+
+    /**
+     * @var Carbon
+     * дата включения валюты в перечень валют, к которым устанавливается официальный курс бел. рубля
+     */
+    protected Carbon $date_start;
+
+    /**
+     * @var Carbon
+     * дата исключения валюты из перечня валют, к которым устанавливается официальный курс бел. рубля
+     */
+    protected Carbon $date_end;
 }
